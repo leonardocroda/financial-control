202f6c954e3ed7837e60bb581a401b5a57f06938
Leonardo Croda <leonardo.croda@hellosaks.com>
2021-08-14 12:40:35 -0300

    creating modules, controllers and services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { CategoriesModule } from './categories/categories.module';
+import { MovementsModule } from './movements/movements.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, CategoriesModule, MovementsModule],
   controllers: [AppController],
   providers: [AppService],
 })
